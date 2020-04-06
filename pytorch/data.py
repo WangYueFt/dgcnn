5dd6f25ad24d57e76d2e5b6228ca875fe2948491
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2020-04-06 23:48:28 +0100

    Stopped h5py producing deprecation warning

--- pytorch/data.py
+++ pytorch/data.py
@@ -36,7 +36,7 @@ def load_data(partition):
     all_data = []
     all_label = []
     for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
-        f = h5py.File(h5_name)
+        f = h5py.File(h5_name, 'r')
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
         f.close()
